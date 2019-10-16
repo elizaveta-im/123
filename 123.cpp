713bbaf1621f4a2e344b6fcab88bd0a9204645ae
elizaveta-im <elizaveta-vasileva-2013@mail.ru>
2019-10-16 22:13:39 +0500

    v4

--- 123.cpp
+++ 123.cpp
@@ -7,6 +7,14 @@ int main(){
    bool IsBreak=false;
    cout<<"Р’РІРµРґРёС‚Рµ"<<endl;
    cin>>str;
-   cout<<"Hello, "<<str<<endl;
+   for (int i=0; str[i]!='\0'; i++)
+   if (str[i]<48 || str[i]>57) {
+      cout<<"Р’РІРµРґРµРЅРѕ РЅРµ С‡РёСЃР»Рѕ\n"; 
+      IsBreak=true; 
+      break;
+   }
+   if(IsBreak==false){
+      cout<<"Hello, "<<str<<endl;
+   }
    return 0;
 }
